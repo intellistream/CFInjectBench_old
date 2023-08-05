7c9d1f40ace538eee6231444a84a8fe4affc2547
mozhu621 <734127782@qq.com>
2023-08-05 15:20:37 +0800

    distillation

--- distillation.py
+++ distillation.py
@@ -3,6 +3,44 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from transformers import AutoTokenizer
+student_id = "google/bert_uncased_L-2_H-128_A-2"
+teacher_id = "textattack/bert-base-uncased-SST-2"
+
+# name for our repository on the hub
+repo_name = "tiny-bert-sst2-distilled"
+
+# init tokenizer
+teacher_tokenizer = AutoTokenizer.from_pretrained(teacher_id)
+student_tokenizer = AutoTokenizer.from_pretrained(student_id)
+
+# sample input
+sample = "This is a basic example, with different words to test."
+
+# assert results
+assert teacher_tokenizer(sample) == student_tokenizer(sample), "Tokenizers haven't created the same output"
+dataset_id="glue"
+dataset_config="sst2"
+
+from datasets import load_dataset
+
+dataset = load_dataset(dataset_id,dataset_config)
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(teacher_id)
+
+def process(examples):
+    tokenized_inputs = tokenizer(
+        examples["sentence"], truncation=True, max_length=512
+    )
+    return tokenized_inputs
+
+tokenized_datasets = dataset.map(process, batched=True)
+tokenized_datasets = tokenized_datasets.rename_column("label","labels")
+
+tokenized_datasets["test"].features
+
 class DistillationTrainingArguments(TrainingArguments):
     def __init__(self, *args, alpha=0.5, temperature=2.0, **kwargs):
         super().__init__(*args, **kwargs)
@@ -38,3 +76,90 @@ class DistillationTrainer(Trainer):
         # Return weighted student loss
         loss = self.args.alpha * student_loss + (1. - self.args.alpha) * loss_logits
         return (loss, outputs_student) if return_outputs else loss
+
+
+from transformers import AutoModelForSequenceClassification, DataCollatorWithPadding
+from huggingface_hub import HfFolder
+
+# create label2id, id2label dicts for nice outputs for the model
+labels = tokenized_datasets["train"].features["labels"].names
+num_labels = len(labels)
+label2id, id2label = dict(), dict()
+for i, label in enumerate(labels):
+    label2id[label] = str(i)
+    id2label[str(i)] = label
+
+# define training args
+training_args = DistillationTrainingArguments(
+    output_dir=repo_name,
+    num_train_epochs=7,
+    per_device_train_batch_size=128,
+    per_device_eval_batch_size=128,
+    fp16=True,
+    learning_rate=6e-5,
+    seed=33,
+    # logging & evaluation strategies
+    logging_dir=f"{repo_name}/logs",
+    logging_strategy="epoch", # to get more information to TB
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    save_total_limit=2,
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy",
+    report_to="tensorboard",
+    # push to hub parameters
+    push_to_hub=True,
+    hub_strategy="every_save",
+    hub_model_id=repo_name,
+    hub_token=HfFolder.get_token(),
+    # distilation parameters
+    alpha=0.5,
+    temperature=4.0
+    )
+
+# define data_collator
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# define model
+teacher_model = AutoModelForSequenceClassification.from_pretrained(
+    teacher_id,
+    num_labels=num_labels,
+    id2label=id2label,
+    label2id=label2id,
+)
+
+# define student model
+student_model = AutoModelForSequenceClassification.from_pretrained(
+    student_id,
+    num_labels=num_labels,
+    id2label=id2label,
+    label2id=label2id,
+)
+
+from datasets import load_metric
+import numpy as np
+
+# define metrics and metrics function
+accuracy_metric = load_metric( "accuracy")
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    acc = accuracy_metric.compute(predictions=predictions, references=labels)
+    return {
+        "accuracy": acc["accuracy"],
+    }
+
+
+trainer = DistillationTrainer(
+    student_model,
+    training_args,
+    teacher_model=teacher_model,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
