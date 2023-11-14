c4efee6ed6b81345fd297a1928a1952ce6d5fd31
mozhu621 <734127782@qq.com>
2023-11-14 11:43:59 +0800

    fig

---
+++ .history/fig_20231114111233.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Data provided by the user
+data = {
+    "dataset": ["Wic", "Rte", "Copa", "hotpot-acc"],
+    "base": [71.15987461, 79.85611511, 54, 46.94119641],
+    "2020": [59.87460815, 51.79856115, 44, 43.68751059],
+    "2021": [57.99, 56.834532, 48, 41.484494],
+    "2022": [59.56112, 53.95, 48, 41.75563464],
+    "2023": [56.42633229, 52.17391, 58, 40.3829]
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Setting the 'dataset' column as the index
+df.set_index('dataset', inplace=True)
+
+# Transposing the DataFrame to have years as the x-axis and datasets as lines
+df = df.transpose()
+
+# Plotting the data
+plt.figure(figsize=(10, 5))
+for column in df.columns:
+    plt.plot(df.index, df[column], marker='o', label=column)
+
+plt.title('Performance over Years')
+plt.xlabel('Year')
+plt.ylabel('Score')
+plt.legend(title='Dataset')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
---
+++ .history/fig_20231114113949.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Data provided by the user
+data = {
+    "dataset": ["Wic", "Rte", "Copa", "hotpot-acc"],
+    "2019(base)": [71.15987461, 79.85611511, 54, 46.94119641],
+    "2020": [59.87460815, 51.79856115, 44, 43.68751059],
+    "2021": [57.99, 56.834532, 48, 41.484494],
+    "2022": [59.56112, 53.95, 48, 41.75563464],
+    "2023": [56.42633229, 52.17391, 58, 40.3829]
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Setting the 'dataset' column as the index
+df.set_index('dataset', inplace=True)
+
+# Transposing the DataFrame to have years as the x-axis and datasets as lines
+df = df.transpose()
+
+# Plotting the data
+plt.figure(figsize=(10, 5))
+print(df)
+plt.plot(df.index, df['Wic'], marker='o', label='Wic', color="salmon")
+
+# Plot for Rte
+plt.plot(df.index, df['Rte'], marker='s', label='Rte', color="khaki")
+
+# Plot for Copa
+plt.plot(df.index, df['Copa'], marker='v', label='Copa', color="lightblue")
+
+# Plot for hotpot-acc (HotpotQA)
+plt.plot(df.index, df['hotpot-acc'], marker='D', label='HotpotQA', color="lightgreen")
+
+
+plt.title('Performance over Years')
+plt.xlabel('Year')
+plt.ylabel('Score')
+plt.legend(title='Dataset')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('./performance_chart.png')
+plt.show()
+
+
+
