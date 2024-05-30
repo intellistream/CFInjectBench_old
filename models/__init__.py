c3577129e02059520250aa8ca1a2fa784aa1c9fe
tongjunshaune <41782238+tongjunshaune@users.noreply.github.com>
2024-05-30 15:41:07 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.T5_Model import T5 as T5_Model
-
-def load_model(type: str):
-    if type=='T5':
-        return T5_Model
-    else:
-        raise Exception('Select the correct model type. Currently supporting only T5.')
